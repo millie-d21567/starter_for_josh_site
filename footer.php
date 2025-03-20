b0d4a0554e80e05306d6c49de6065b67b4bc6b76
millie-d21567 <34613846@student.murdoch.edu.au>
2025-03-20 17:02:21 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,4 +1,3 @@
-
 <footer class="container-fluid text-center">
     <div class="container">
         <div class="row">
@@ -6,12 +5,13 @@
                 <a class="navbar-brand" href="#"><span class="josh">JOSH</span><span class="orange">WHITKIN</span></a>
             </div>
             <div class="col-md-6">
+                <!-- The image element -->
+                <img class="img-fluid" alt="footer image" src="http://170.187.231.66/~mesh9/wp-content/themes/starter_for_josh_site/images/footer.jpg">
                 <p>Terms and Conditions</p>
             </div>
         </div> <!-- row -->
     </div> <!-- container -->
 </footer>
 <?php wp_footer(); ?>
-
 </body>
 </html>
