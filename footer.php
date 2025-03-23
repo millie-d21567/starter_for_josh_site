af24af5a78f8778c348eefa5925e79f6e52b90b3
millie-d21567 <34613846@student.murdoch.edu.au>
2025-03-23 20:24:02 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,17 +1,18 @@
-<footer class="container-fluid text-center">
-    <div class="row">
-        <a class="navbar-brand" href="#"><span class="josh">JOSH</span><span class="orange">WHITKIN</span></a>
-    </div>
-    <div class="position-relative">
-        <!-- The image element -->
-        <img class="img-fluid w-100" alt="footer image" src="http://170.187.231.66/~mesh9/wp-content/themes/starter_for_josh_site/Images/footer.jpg">
-        <!-- The text overlay -->
-        <div class="overlay-text">
-            <p>Terms and Conditions</p>
-        </div>
-    </div>
-</footer>
+<footer class="container-fluid text-center"><!-- div for the main image to stretch s to the edge of the page-->
 
+<div class="container"><!-- div to hold the content in the middle of the page-->
+    <div class="row"><!-- a row that gives us access to the BS columns-->
+        <div class="col-md-6 ">
+           <a class="navbar-brand" href="#"><span class="josh">JOSH</span> <span class="orange">WHITKIN</span> </a>
+
+        </div>
+        <div class="col-md-6 ">
+            <p>Terms and Conditions</p>
+</div><!-- row-->
+    </div><!-- container -->
+</div><!-- container-fluid-->
+
+</footer>
 <?php wp_footer(); ?>
 </body>
 </html>
