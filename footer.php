4b1f7879eb9514082e8c987250e03e472f1f8cb2
millie-d21567 <34613846@student.murdoch.edu.au>
2025-03-23 09:23:00 +0800

    Update footer.php
    getting rid of break

--- footer.php
+++ footer.php
@@ -1,17 +1,17 @@
 <footer class="container-fluid text-center">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-6">
-                <a class="navbar-brand" href="#"><span class="josh">JOSH</span><span class="orange">WHITKIN</span></a>
-            </div>
-            <div class="col-md-6">
-                <!-- The image element -->
-                <img class="img-fluid" alt="footer image" src="http://170.187.231.66/~mesh9/wp-content/themes/starter_for_josh_site/images/footer.jpg">
-                <p>Terms and Conditions</p>
-            </div>
-        </div> <!-- row -->
-    </div> <!-- container -->
+    <div class="row">
+        <a class="navbar-brand" href="#"><span class="josh">JOSH</span><span class="orange">WHITKIN</span></a>
+    </div>
+    <div class="position-relative">
+        <!-- The image element -->
+        <img class="img-fluid w-100" alt="footer image" src="http://170.187.231.66/~mesh9/wp-content/themes/starter_for_josh_site/Images/footer.jpg">
+        <!-- The text overlay -->
+        <div class="overlay-text">
+            <p>Terms and Conditions</p>
+        </div>
+    </div>
 </footer>
+
 <?php wp_footer(); ?>
 </body>
 </html>
