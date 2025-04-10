0a4bacc527d08d6afc249964b368377f7e450e66
millie-d21567 <34613846@student.murdoch.edu.au>
2025-04-10 17:15:38 +0800

    Update page_header.php

--- page_header.php
+++ page_header.php
@@ -1,87 +1,95 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="description" content="Enter your description here">
-    
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@800&display=swap" rel="stylesheet">
-    
-    <title><?php
-        if (function_exists('is_tag') && is_tag()) {
-           single_tag_title("Tag Archive for &quot;"); echo '&quot; - '; }
-        elseif (is_archive()) {
-           wp_title(''); echo ' Archive - '; }
-        elseif (is_search()) {
-           echo 'Search for &quot;'.esc_html(get_search_query()).'&quot; - '; }
-        elseif (!(is_404()) && (is_single()) || (is_page())) {
-           wp_title(''); echo ' - '; }
-        elseif (is_404()) {
-           echo 'Not Found - '; }
-        if (is_home()) {
-           bloginfo('name'); echo ' - '; bloginfo('description'); }
-        else {
-            bloginfo('name'); }
-        if ($paged>1) {
-           echo ' - page '. $paged; }
-     ?>
-    </title>
+<meta charset="<?php bloginfo('charset'); ?>"> 
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<meta name="pls stop causing w3c errors" content="idk">
 
-    <meta name="description" content="<?php
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,800;1,800&display=swap" rel="stylesheet">
+<title><?php
+              if (function_exists('is_tag') && is_tag()) {
+                 single_tag_title("Tag Archive for &quot;"); echo '&quot; - '; }
+              elseif (is_archive()) {
+                 wp_title(''); echo ' Archive - '; }
+              elseif (is_search()) {
+                 echo 'Search for &quot;'.wp_specialchars($s).'&quot; - '; }
+              elseif (!(is_404()) && (is_single()) || (is_page())) {
+                 wp_title(''); echo ' - '; }
+              elseif (is_404()) {
+                 echo 'Not Found - '; }
+              if (is_home()) {
+                 bloginfo('name'); echo ' - '; bloginfo('description'); }
+              else {
+                  bloginfo('name'); }
+              if ($paged>1) {
+                 echo ' - page '. $paged; }
+           ?>
+</title>
+<meta name="description" content="<?php
     if (is_single() || is_page()) {
-      $excerpt = get_the_excerpt();
-      if (empty($excerpt)) {
-          $excerpt = wp_trim_words(get_the_content(), 25);
-      }
-      if (empty($excerpt)) {
-          $excerpt = get_bloginfo('description');
-      }
-      $meta_description = mb_substr(wp_strip_all_tags($excerpt), 0, 160);
-      echo esc_attr($meta_description);
+        $excerpt = get_the_excerpt();
+        
+        // Fallback to content if excerpt is empty
+        if (empty($excerpt)) {
+            $excerpt = wp_trim_words(get_the_content(), 25);
+        }
+        
+        // Final fallback to site description if both excerpt and content are empty
+        if (empty($excerpt)) {
+            $excerpt = get_bloginfo('description');
+        }
+        
+        // Limit the description to 160 characters
+        $meta_description = mb_substr(wp_strip_all_tags($excerpt), 0, 160);
+        
+        echo esc_attr($meta_description);
     } else {
-      echo esc_attr(get_bloginfo('description'));
+        echo esc_attr(get_bloginfo('description'));
     }
-    ?>">
-
-    <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/images/favicon.ico">
+?>
+<link rel="shortcut icon" href="<?php echo get_bloginfo('template_directory'); ?>/_/img/favicon.ico">
+<meta name="description" content="<?php bloginfo('description'); ?>">
 </head>
-
 <body>
 
-<nav class="navbar navbar-expand-md navbar-custom">
-    <a class="navbar-brand" href="#"><span class="josh">JOSH</span><span class="orange">WHITKIN</span></a>
-    
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    
-    <div class="collapse navbar-collapse" id="collapsibleNavbar">
-        <ul class="navbar-nav ml-auto">
-            <li class="nav-item text-right">
-                <a class="nav-link" href="#">HOME</a>
+<header class="container-fluid smallpage-bg">
+    <div class="container">
+    <nav class="navbar navbar-expand-md navbar-dark">
+        <!-- Brand -->
+        <a class="navbar-brand" href="#"><span class="josh">JOSH</span><span class="orange"> WHITKIN</span></a>
+      
+        <!-- Toggler/collapsibe Button -->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      
+        <!-- Navbar links -->
+        <div class="collapse navbar-collapse" id="collapsibleNavbar">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="#">HOME</a>
             </li>
-            <li class="nav-item text-right">
-                <a class="nav-link" href="#">PROJECTS</a>
+            <li class="nav-item">
+              <a class="nav-link" href="#">PROJECTS</a>
             </li>
-            <li class="nav-item text-right">
-                <a class="nav-link" href="#">CV</a>
+            <li class="nav-item">
+              <a class="nav-link" href="#">CV</a>
             </li>
-            <li class="nav-item text-right">
+            <li class="nav-item">
                 <a class="nav-link" href="#">CONTACT</a>
-            </li>
-        </ul>
-    </div>
-</nav>
-    
-<div class="container fluid smallpage-bg"><!-- div that holds the content in the middle of the page-->
-        <h1 class="smallpage-title"> Josh’s CV</h1>
-        </div><!-- container-->
+              </li>
+          </ul>
+        </div>
+      </nav>
 
-</div>
-<?php wp_head(); ?>
-</header>
+      <div class="smallpage-tag">
+      <h1 class="smallpage-title">Projects</h1>
+    </div>
+
+    </div> <!-- container -->
+    <?php wp_head(); ?> 
+    </header>
