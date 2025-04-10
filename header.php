7731ea1be6db11ef3d029bb72a33b087621129ea
millie-d21567 <34613846@student.murdoch.edu.au>
2025-04-10 17:15:32 +0800

    Update header.php

--- header.php
+++ header.php
@@ -1,69 +1,66 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@800&display=swap" rel="stylesheet">
-    
-    <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/images/favicon.ico">
+<meta charset="<?php bloginfo('charset'); ?>"> 
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<meta name="pls stop causing w3c errors" content="idk">
 
-    <title>
-        <?php
-            if (function_exists('is_tag') && is_tag()) {
-                single_tag_title("Tag Archive for &quot;"); echo '&quot; - '; 
-            } elseif (is_archive()) {
-                wp_title(''); echo ' Archive - '; 
-            } elseif (is_search()) {
-                echo 'Search for &quot;'.esc_html(get_search_query()).'&quot; - '; 
-            } elseif (!(is_404()) && (is_single()) || (is_page())) {
-                wp_title(''); echo ' - '; 
-            } elseif (is_404()) {
-                echo 'Not Found - '; 
-            }
-            if (is_home()) {
-                bloginfo('name'); echo ' - '; bloginfo('description'); 
-            } else {
-                bloginfo('name'); 
-            }
-            if (get_query_var('paged') > 1) {
-                echo ' - page '. get_query_var('paged'); 
-            }
-        ?>
-    </title>
 
-    <meta name="description" content="<?php
-        if (is_single() || is_page()) {
-            $excerpt = get_the_excerpt();
-            
-            // Fallback to content if excerpt is empty
-            if (empty($excerpt)) {
-                $excerpt = wp_trim_words(get_the_content(), 25);
-            }
-            
-            // Final fallback to site description if both excerpt and content are empty
-            if (empty($excerpt)) {
-                $excerpt = get_bloginfo('description');
-            }
-            
-            // Limit the description to 160 characters
-            $meta_description = mb_substr(wp_strip_all_tags($excerpt), 0, 160);
-            
-            echo esc_attr($meta_description);
-        } else {
-            echo esc_attr(get_bloginfo('description'));
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,800;1,800&display=swap" rel="stylesheet">
+<title><?php
+              if (function_exists('is_tag') && is_tag()) {
+                 single_tag_title("Tag Archive for &quot;"); echo '&quot; - '; }
+              elseif (is_archive()) {
+                 wp_title(''); echo ' Archive - '; }
+              elseif (is_search()) {
+                 echo 'Search for &quot;'.wp_specialchars($s).'&quot; - '; }
+              elseif (!(is_404()) && (is_single()) || (is_page())) {
+                 wp_title(''); echo ' - '; }
+              elseif (is_404()) {
+                 echo 'Not Found - '; }
+              if (is_home()) {
+                 bloginfo('name'); echo ' - '; bloginfo('description'); }
+              else {
+                  bloginfo('name'); }
+              if ($paged>1) {
+                 echo ' - page '. $paged; }
+           ?>
+</title>
+<meta name="description" content="<?php
+    if (is_single() || is_page()) {
+        $excerpt = get_the_excerpt();
+        
+        // Fallback to content if excerpt is empty
+        if (empty($excerpt)) {
+            $excerpt = wp_trim_words(get_the_content(), 25);
         }
-    ?>">
-
+        
+        // Final fallback to site description if both excerpt and content are empty
+        if (empty($excerpt)) {
+            $excerpt = get_bloginfo('description');
+        }
+        
+        // Limit the description to 160 characters
+        $meta_description = mb_substr(wp_strip_all_tags($excerpt), 0, 160);
+        
+        echo esc_attr($meta_description);
+    } else {
+        echo esc_attr(get_bloginfo('description'));
+    }
+?>
+<link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/_/img/favicon.ico">
+<meta name="description" content="<?php bloginfo('descrption'); ?>">
 </head>
 <body>
 
-<nav class="navbar navbar-expand-md navbar-custom">
+<header class="container-fluid" style="background-size: cover; background-image: url(<?php the_field('headerbackgroundimage') ?>);">
+    <div class="container">
+    <nav class="navbar navbar-expand-md navbar-dark">
         <!-- Brand -->
-        <a class="navbar-brand" href="http://206.189.45.97/~mesh9/"><span class="josh">JOSH</span><span class="orange">WHITKIN</span></a>
+        <a class="navbar-brand" href="http://206.189.45.97/~mesh9/"><span class="josh">JOSH</span><span class="orange"> WHITKIN</span></a>
       
         <!-- Toggler/collapsibe Button -->
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'your-theme-slug' ); ?>">
@@ -87,14 +84,10 @@ wp_nav_menu( array(
 
       </nav> 
 
-      <header class="container-fluid" style="background-size: cover; background-image: url(<?php the_field('headerbackgroundimage') ?>);">
-    <div class="tag">
-    <div class="header-image">
-    <div class="tag"><!-- div that holds the content in the middle of the page-->
-        <h1><?php the_field('maintagline'); ?></h1>
-              </div><!-- container-->
+      <div class="tag">
+      <h1><?php the_field('maintagline'); ?></h1>
+    </div>
 
-    </div>
-    </div>
-<?php wp_head(); ?>
-</header>
+    </div> <!-- container -->
+    <?php wp_head(); ?> 
+    </header>
