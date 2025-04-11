f17679253fd91c142d83516f6970e08d711452dc
millie-d21567 <34613846@student.murdoch.edu.au>
2025-04-11 14:11:53 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 <div class="container"><!-- div to hold the content in the middle of the page-->
     <div class="row"><!-- a row that gives us access to the BS columns-->
         <div class="col-md-6 ">
-           <a class="navbar-brand" href="#"><span class="josh">JOSH</span> <span class="orange">WHITKIN</span> </a>
+           <a class="navbar-brand" href="http://170.187.231.66/~mesh9"><span class="josh">JOSH</span> <span class="orange">WHITKIN</span> </a>
 
         </div>
         <div class="col-md-6 ">
