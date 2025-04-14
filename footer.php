93cc58c903b3e2ed6d8e762c68227985a596ceb9
millie-d21567 <34613846@student.murdoch.edu.au>
2025-04-14 16:24:57 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -3,11 +3,18 @@
 <div class="container"><!-- div to hold the content in the middle of the page-->
     <div class="row"><!-- a row that gives us access to the BS columns-->
         <div class="col-md-6 ">
-           <a class="navbar-brand" href="http://170.187.231.66/~mesh9"><span class="josh">JOSH</span> <span class="orange">WHITKIN</span> </a>
+           <!-- <a class="navbar-brand" href="http://170.187.231.66/~mesh9"><span class="josh">JOSH</span> <span class="orange">WHITKIN</span> </a> -->
+           <div class="col-md-6 widgetleft">
+                    
+            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-left') ) : 
+endif; ?>
+
 
         </div>
         <div class="col-md-6 ">
-            <p>Terms and Conditions</p>
+        <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-right') ) : 
+endif; ?>
+            <!-- <p>Terms and Conditions</p> -->
 </div><!-- row-->
     </div><!-- container -->
 </div><!-- container-fluid-->
